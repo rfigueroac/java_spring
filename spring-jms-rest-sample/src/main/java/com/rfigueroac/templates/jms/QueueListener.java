3407c329c8eb26ff9fefe4a4c0af517ee141499d
c0712c3809ca35c2d8143bd39f06b2e8 <randresfigueroa@gmail.com>
2012-02-13 03:38:32 -0500

    Agregando La configuracion part 2

---
+++ spring-jms-rest-sample/src/main/java/com/rfigueroac/templates/jms/QueueListener.java
@@ -0,0 +1,41 @@
+package com.rfigueroac.templates.jms;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.apache.log4j.Logger;
+import org.springframework.jms.core.JmsTemplate;
+
+/**
+ * 
+ * @author lshimokawa
+ * 
+ */
+public class QueueListener implements MessageListener {
+	private static final Logger logger = Logger.getLogger(QueueListener.class);
+
+	private JmsTemplate jmsTemplate;
+	private String responseDestination;
+
+	public void onMessage(final Message message) {
+		if (message instanceof TextMessage) {
+			final TextMessage textMessage = (TextMessage) message;
+			try {
+				logger.debug("QueueListener:" + textMessage.getText());
+			} catch (final JMSException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void setJmsTemplate(JmsTemplate jmsTemplate) {
+		this.jmsTemplate = jmsTemplate;
+	}
+
+	public void setResponseDestination(String responseDestination) {
+		this.responseDestination = responseDestination;
+	}
+
+}
